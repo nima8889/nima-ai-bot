13e6c091c35bb7252f7b7eb8c65ea40cf821724f
nima8889 <Nimar1a3h8i8mi@gmail.com>
2026-09-13 21:47:15 +0330

    /src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-git clone https://x:art_v2_x_a7e1912b68ce4ce41d7ce1fe6c5f2ab6a1fd9758@31b91e7f9954ad8aa334d46f012bd8ed.artifacts.cloudflare.net/git/lee-production/lee-nima-ai-bot-deploy-563f1380.git nima-ai-bot-deploy
+git clone https://x:art_v2_x_cf63ba2ced16968fb7a74d851429ad785856f71c@31b91e7f9954ad8aa334d46f012bd8ed.artifacts.cloudflare.net/git/lee-production/lee-nima-ai-bot-final-574ce914.git nima-ai-bot-final
